1eb9615247892050737768e82b92ed2109b5363d
Daniel D <daniel.diepold@gmail.com>
2011-03-14 23:23:09 +0100

    miniclient update

--- scripts/miniclient/miniclient.c
+++ scripts/miniclient/miniclient.c
@@ -2,66 +2,98 @@
 #include <mysql.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 
 /* stored procedure:
  *  CREATE FUNCTION getJob(idExperiment INT) RETURNS INT BEGIN DECLARE job_id INT; DECLARE tmp INT; set job_id = -1; SELECT FLOOR(RAND()*COUNT(idJob)) into tmp FROM ExperimentResults WHERE Experiment_idExperiment=idExperiment AND status<0; SELECT idJob into job_id FROM ExperimentResults WHERE Experiment_idExperiment=idExperiment and status <0 LIMIT tmp,1 FOR UPDATE; UPDATE ExperimentResults SET status=0 WHERE idJob=job_id; return job_id; END;
-  */
+ */
 
-const char * LOCK_JOB = "UDPATE ExperimentResults SET status=0 WHERE idJob=%d;";
+const char * LOCK_JOB = "UDPATE ExperimentResults SET status=0 WHERE status<0 AND idJob=%d;";
 
-/*int fetchJob_RANDOM_LIMIT(MYSQL* conn, int experiment_id) { // buggy (doesn't process all jobs)
-    const char* SELECT_Q = "SELECT idJob FROM ExperimentResults WHERE status < 0 AND Experiment_idExperiment = %d LIMIT %d,1 FOR UPDATE;";
-    char* query = calloc(1, 512);
-    sprintf(query, SELECT_Q, experiment_id, rand() % 2000);
-    if (mysql_query(conn, query) != 0) {
-        printf("%s\n", mysql_error(conn));
-        exit(1);
-    }
+int fetchJob_RANDOM_LIMIT(MYSQL* conn, int experiment_id) {
+    MYSQL_RES* result;
+    char* query;
+    /*const char* TEST_FREE = "SELECT idJob FROM ExperimentResults WHERE status < 0 AND Experiment_idExperiment = %d LIMIT 1;";
+    query = calloc(1, 512);
+    sprintf(query, TEST_FREE, experiment_id);
+    mysql_query(conn, query);
     free(query);
     MYSQL_RES* result = mysql_store_result(conn);
     if (mysql_num_rows(result) < 1) {
         mysql_free_result(result);
         return -1;
     }
+    mysql_free_result(result);*/
+
+    query = calloc(1, 512);
+    const char* SELECT_Q = "SELECT idJob FROM ExperimentResults WHERE status < 0 AND Experiment_idExperiment = %d LIMIT %d,1;";
+    if (rand() % 100 < 20) {
+        sprintf(query, SELECT_Q, experiment_id, 0);
+    } else {
+        sprintf(query, SELECT_Q, experiment_id, rand() % 1000);
+    }
+    if (mysql_query(conn, query) != 0) {
+        printf("%s\n", mysql_error(conn));
+        exit(1);
+    }
+    free(query);
+    result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        return -2;
+    }
     MYSQL_ROW row = mysql_fetch_row(result);
     mysql_free_result(result);
     int idJob = atoi(row[0]);
-    
+
+    const char * LOCK_QUERY = "SELECT idJob FROM ExperimentResults WHERE idJob = %d and status<0 FOR UPDATE;";
+    query = calloc(1, 256);
+    sprintf(query, LOCK_QUERY, idJob);
+    mysql_query(conn, query);
+    free(query);
+    result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        mysql_commit(conn);
+        return -2; // job was taken by another client in the meantime
+    }
+    mysql_free_result(result);
+
+
+
     query = calloc(1, 256);
     sprintf(query, LOCK_JOB, idJob);
     mysql_query(conn, query);
     free(query);
     mysql_commit(conn);
     return idJob;
-}*/
+}
 
 /* int fetchJob_UPDATE(MYSQL* conn, int experiment_id) { // buggy
-    char * UPDATE_QUERY = "UPDATE ExperimentResults SET status=0 WHERE Experiment_idExperiment=%d AND status=-1 AND idJob = (@update_id := idJob) LIMIT 1;";
-    char * query = calloc(1, 256);
-    sprintf(query, UPDATE_QUERY, experiment_id);
-    mysql_query(conn, query);
-    free(query);
-    char * SELECT_QUERY = "SELECT @update_id;";
-    query = calloc(1, 256);
-    sprintf(query, SELECT_QUERY);
-    mysql_query(conn, query);
-    mysql_commit(conn);
-    free(query);
-    MYSQL_RES* result = mysql_store_result(conn);
-    if (mysql_num_rows(result) < 1) {
-        mysql_free_result(result);
-        return -1;
-    }
-    MYSQL_ROW row = mysql_fetch_row(result);
-    int idJob = atoi(row[0]);
-    mysql_free_result(result);
-    return idJob;
-}*/
+   char * UPDATE_QUERY = "UPDATE ExperimentResults SET status=0 WHERE Experiment_idExperiment=%d AND status=-1 AND idJob = (@update_id := idJob) LIMIT 1;";
+   char * query = calloc(1, 256);
+   sprintf(query, UPDATE_QUERY, experiment_id);
+   mysql_query(conn, query);
+   free(query);
+   char * SELECT_QUERY = "SELECT @update_id;";
+   query = calloc(1, 256);
+   sprintf(query, SELECT_QUERY);
+   mysql_query(conn, query);
+   mysql_commit(conn);
+   free(query);
+   MYSQL_RES* result = mysql_store_result(conn);
+   if (mysql_num_rows(result) < 1) {
+   mysql_free_result(result);
+   return -1;
+   }
+   MYSQL_ROW row = mysql_fetch_row(result);
+   int idJob = atoi(row[0]);
+   mysql_free_result(result);
+   return idJob;
+   }*/
 
 int fetchJob_FLOOR_LOCK(MYSQL* conn, int experiment_id) {
     const char* SELECT_QUERY = "SELECT FLOOR(RAND()*COUNT(idJob)) as x FROM ExperimentResults \
-                                 WHERE Experiment_idExperiment=%d AND status < 0;";
+                                WHERE Experiment_idExperiment=%d AND status < 0;";
     char* query = calloc(1, 256);
     sprintf(query, SELECT_QUERY, experiment_id);
     mysql_query(conn, query);
@@ -76,7 +108,7 @@ int fetchJob_FLOOR_LOCK(MYSQL* conn, int experiment_id) {
     int limit = atoi(row[0]);
 
     const char * LOCK_QUERY = "SELECT idJob FROM ExperimentResults WHERE Experiment_idExperiment=%d \
-                             AND status < 0 LIMIT %d,1 FOR UPDATE;";
+                               AND status < 0 LIMIT %d,1 FOR UPDATE;";
 
     query = calloc(1, 256);
     sprintf(query, LOCK_QUERY, experiment_id, limit);
@@ -90,7 +122,7 @@ int fetchJob_FLOOR_LOCK(MYSQL* conn, int experiment_id) {
     row = mysql_fetch_row(result);
     mysql_free_result(result);
     int idJob = atoi(row[0]);
-    
+
     query = calloc(1, 256);
     sprintf(query, LOCK_JOB, idJob);
     mysql_query(conn, query);
@@ -98,6 +130,60 @@ int fetchJob_FLOOR_LOCK(MYSQL* conn, int experiment_id) {
     mysql_commit(conn);
     return idJob;
 }
+
+int fetchJob_FLOOR_LOCK2(MYSQL* conn, int experiment_id) {
+    const char* SELECT_QUERY = "SELECT FLOOR(RAND()*COUNT(idJob)) as x FROM ExperimentResults \
+                                WHERE Experiment_idExperiment=%d AND status < 0;";
+    char* query = calloc(1, 256);
+    sprintf(query, SELECT_QUERY, experiment_id);
+    mysql_query(conn, query);
+    free(query);
+    MYSQL_RES* result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        return -1;
+    }
+    MYSQL_ROW row = mysql_fetch_row(result);
+    mysql_free_result(result);
+    int limit = atoi(row[0]);
+
+    const char * ID_QUERY = "SELECT idJob FROM ExperimentResults WHERE Experiment_idExperiment=%d \
+                             AND status < 0 LIMIT %d,1;";
+
+    query = calloc(1, 256);
+    sprintf(query, ID_QUERY, experiment_id, limit);
+    mysql_query(conn, query);
+    free(query);
+    result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        return -1;
+    }
+    row = mysql_fetch_row(result);
+    mysql_free_result(result);
+    int idJob = atoi(row[0]);
+
+    const char * LOCK_QUERY = "SELECT idJob FROM ExperimentResults WHERE idJob = %d and status<0 FOR UPDATE;";
+    query = calloc(1, 256);
+    sprintf(query, LOCK_QUERY, idJob);
+    mysql_query(conn, query);
+    free(query);
+    result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        mysql_commit(conn);
+        return -2; // job was taken by another client between the 2 queries
+    }
+    mysql_free_result(result);
+
+    query = calloc(1, 256);
+    sprintf(query, LOCK_JOB, idJob);
+    mysql_query(conn, query);
+    free(query);
+    mysql_commit(conn);
+    return idJob;
+}
+
 
 int fetchJob_STORED_PROC(MYSQL* conn, int experiment_id) {
     const char* SELECT_QUERY = "SELECT getJob(%d);";
@@ -130,7 +216,7 @@ int fetchJob_ORDER_BY_RAND(MYSQL* conn, int experiment_id) {
     MYSQL_ROW row = mysql_fetch_row(result);
     int idJob = atoi(row[0]);
     mysql_free_result(result);
-    
+
     query = calloc(1, 256);
     sprintf(query, LOCK_JOB, idJob);
     mysql_query(conn, query);
@@ -139,10 +225,46 @@ int fetchJob_ORDER_BY_RAND(MYSQL* conn, int experiment_id) {
     return idJob;
 }
 
+int fetchJob_ORDER_BY_RAND2(MYSQL* conn, int experiment_id) {
+    const char * SINGLE_LOCK_QUERY = "SELECT idJob FROM ExperimentResults WHERE Experiment_idExperiment=%d \
+                                      AND status<0 ORDER BY RAND() LIMIT 1;";
+    char * query = calloc(1, 256);
+    sprintf(query, SINGLE_LOCK_QUERY, experiment_id);
+    mysql_query(conn, query);
+    free(query);
+    MYSQL_RES* result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        return -1;
+    }
+    MYSQL_ROW row = mysql_fetch_row(result);
+    int idJob = atoi(row[0]);
+    mysql_free_result(result);
+
+    const char * LOCK_QUERY = "SELECT idJob FROM ExperimentResults WHERE idJob = %d and status<0 FOR UPDATE;";
+    query = calloc(1, 256);
+    sprintf(query, LOCK_QUERY, idJob);
+    mysql_query(conn, query);
+    free(query);
+    result = mysql_store_result(conn);
+    if (mysql_num_rows(result) < 1) {
+        mysql_free_result(result);
+        return -2; // job was taken by another client between the 2 queries
+    }
+    mysql_free_result(result);
+
+    query = calloc(1, 256);
+    sprintf(query, LOCK_JOB, idJob);
+    mysql_query(conn, query);
+    free(query);
+    mysql_commit(conn);
+    return idJob;
+}
+
+
 int fetchJob_FIRST_FREE(MYSQL* conn, int experiment_id) {
     char * query = calloc(1, 256);
-    sprintf(query, "SELECT idJob FROM ExperimentResults WHERE status<0 AND \
-            Experiment_idExperiment=%d LIMIT 1 FOR UPDATE", experiment_id);
+    sprintf(query, "SELECT idJob FROM ExperimentResults WHERE Experiment_idExperiment=%d AND status<0 LIMIT 1 FOR UPDATE", experiment_id);
     mysql_query(conn, query);
     free(query);
     MYSQL_RES* result = mysql_store_result(conn);
@@ -172,10 +294,8 @@ void processJob(MYSQL* conn, int idJob) {
 }
 
 int num_jobs(MYSQL* conn, int experiment_id, int status) {
-    usleep((rand() % 1000) * 1000); // job processing time between 0 and 1000 ms
     char* query = calloc(1, 256);
-    sprintf(query, "SELECT COUNT(idJob) FROM ExperimentResults WHERE Experiment_idExperiment=%d \
-            AND status=%d;", experiment_id, status);
+    sprintf(query, "SELECT COUNT(idJob) FROM ExperimentResults WHERE Experiment_idExperiment=%d AND status=%d;", experiment_id, status);
     mysql_query(conn, query);
     free(query);
     MYSQL_RES* result = mysql_store_result(conn);
@@ -204,6 +324,10 @@ int main(int argc, char* argv[]) {
     else if (job_method == 1) fetchJob = fetchJob_ORDER_BY_RAND;
     else if (job_method == 2) fetchJob = fetchJob_FIRST_FREE;
     else if (job_method == 3) fetchJob = fetchJob_STORED_PROC;
+    else if (job_method == 4) fetchJob = fetchJob_RANDOM_LIMIT;
+    else if (job_method == 5) fetchJob = fetchJob_FLOOR_LOCK2;
+    else if (job_method == 6) fetchJob = fetchJob_ORDER_BY_RAND2;
+
 
     MYSQL* conn = mysql_init(NULL);
     if (mysql_real_connect(conn, db_host, db_user, db_passw, db_name, 3306, NULL, 0) == NULL) {
@@ -211,7 +335,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     mysql_close(conn);
-    
+
     srand(time(0));
     int pid = -1;
     for (int i = 0; i < num_clients; i++) {
@@ -220,7 +344,8 @@ int main(int argc, char* argv[]) {
     }
 
     if (pid == 0) { // child, process jobs
-        srand(time(0));
+        srand(getpid());
+        usleep((rand() % 1000) * 5000);
         conn = mysql_init(NULL);
         if (mysql_real_connect(conn, db_host, db_user, db_passw, db_name, 3306, NULL, 0) == NULL) {
             printf("Error connecting to DB: %s\n", mysql_error(conn));
@@ -228,7 +353,10 @@ int main(int argc, char* argv[]) {
         }
         mysql_autocommit(conn, 0);
         int idJob;
-        while ((idJob = fetchJob(conn, experiment_id)) >= 0) {
+        while ((idJob = fetchJob(conn, experiment_id)) != -1) {
+            if (idJob == -2) { 
+                usleep(200 * 1000); continue;  // sleep 200ms
+            }
             processJob(conn, idJob);
         }
     }
@@ -239,14 +367,16 @@ int main(int argc, char* argv[]) {
             exit(1);
         }
         int finished = num_jobs(conn, experiment_id, 1);
-        int time = 0;
+        int elapsed_time = 0;
+        time_t t_last_update = time(0);
         while (1) {
             sleep(2);
             int finished_now = num_jobs(conn, experiment_id, 1);
             int delta = finished_now - finished;
-            printf("Time elapsed: %d - Jobs/sec: %.2f\n", time, delta / 2.0f);
+            printf("Time elapsed: %d sec - Jobs/sec: %.2f\n", elapsed_time, delta / (double)(time(0) - t_last_update));
             finished = finished_now;
-            time += 2;
+            elapsed_time += time(0) - t_last_update;
+            t_last_update = time(0);
             if (num_jobs(conn, experiment_id, 0) == 0 && num_jobs(conn, experiment_id, -1) == 0) {
                 break;
             }
