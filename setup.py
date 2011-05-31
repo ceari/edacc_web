b7ba56eaa1ce84bd2a8ade9305e7d30980bc0a41
Daniel D <daniel.diepold@gmail.com>
2011-05-31 20:06:34 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="edacc_web",
+    version="1.0",
+    author="Daniel Diepold",
+    long_description="EDACC Web Frontend",
+    packages=["edacc", "edacc.views"],
+    include_package_data=True,
+    platforms="any",
+    zip_safe=False,
+    install_requires=[
+        "Flask>=0.6",
+        "SQLAlchemy>=0.6.5",
+        "rpy2>=2.1",
+        "mysql-python>=1.2",
+        "Flask-WTF>=0.5.2",
+        "Flask-Actions>=0.5.2",
+        "PyLZMA>=0.4.2",
+    ]
+)
