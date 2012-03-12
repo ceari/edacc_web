520908ce8ff02b423756c6240f3fcea11e864a43
Daniel D <daniel.diepold@gmail.com>
2012-03-12 12:37:02 +0100

    show instance details also in testing phase

--- edacc/constants.py
+++ edacc/constants.py
@@ -68,7 +68,7 @@ for sig in range(1, 31):
 
 OWN_RESULTS = set([2, 3, 4, 5])        # phases where own results are shown
 ALL_RESULTS = set([6, 7])           # phases where all results are shown
-INSTANCE_DETAILS = set([6, 7])      # phases where full access to instance details/downloads is granted
+INSTANCE_DETAILS = set([2, 6, 7])      # phases where full access to instance details/downloads is granted
 ANALYSIS1 = set([5, 6, 7])          # phases where some plots are shown (cactus)
 ANALYSIS2 = set([6, 7])             # phases where the other plots are shown
 RANKING = set([2, 3, 4, 5, 6, 7])               # phases where ranking is shown
