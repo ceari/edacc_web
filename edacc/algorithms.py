35d1f2207f1f304721503fd3b1279b11e45eb86d
Daniel D <daniel.diepold@gmail.com>
2012-06-18 14:15:41 +0200

    -

---
+++ edacc/algorithms.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+sets_union = lambda sets: reduce(lambda x,y: x.union(y), sets, set())
+
+def min_set_cover(U, sets):
+    # brute force implementation ...
+    min_combinations = list()
+    for size in range(len(sets)):
+        for comb in combinations(sets, size):
+            if len(U.difference(sets_union(comb))) == 0:
+                if comb not in min_combinations:
+                    min_combinations.append(comb)
+        if min_combinations: break
+    return min_combinations
