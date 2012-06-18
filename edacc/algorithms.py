aae6ec5550301397c35ff34a7cd3271f246b77fb
Daniel D <daniel.diepold@gmail.com>
2012-06-18 14:22:48 +0200

    bugfix

--- edacc/algorithms.py
+++ edacc/algorithms.py
@@ -5,7 +5,7 @@ sets_union = lambda sets: reduce(lambda x,y: x.union(y), sets, set())
 def min_set_cover(U, sets):
     # brute force implementation ...
     min_combinations = list()
-    for size in range(len(sets)):
+    for size in range(1, len(sets)+1):
         for comb in combinations(sets, size):
             if len(U.difference(sets_union(comb))) == 0:
                 if comb not in min_combinations:
